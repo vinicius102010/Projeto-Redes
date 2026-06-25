3e68f8f5222304ed6f63b17a8fbae64f97ea72fe
Vinicius Chaves <vinycius102010@gmail.com>
2026-06-25 16:24:31 -0300

    fix: corrige imports desnecessarios

--- main.py
+++ main.py
@@ -1,33 +1,31 @@
-import hashlib
-import json
-import os
-import random
 import socket
+import os
+import json
 import threading
-import time
+import hashlib
 import uuid
+import time
+import struct
 
-# configs e estado global
-PORTA_BROADCAST = 50000
+#TODO: testar fora do docker
 IP_BROADCAST =  os.getenv("IP_BROADCAST","127.255.255.255")
 PASTA_COMPARTILHADA = "./one_piece_drive"
-meu_id = str(uuid.uuid4())[:8]
-nos_conhecidos = {}  # Formato: {'id': {'ip': '...', 'tcp_port': 1234, 'ultima_vez': ...}}
-lock = threading.Lock()
-meu_tcp_port = 0
+meu_id = str(uuid.uuid4())[:8] #corta o id pra nao ficar gigante
+minha_porta_tcp = 0
 socket_tracker_global = None
 TRACKER_HOST = os.getenv("TRACKER_HOST", "127.0.0.1")
 TRACKER_PORT = int(os.getenv("TRACKER_PORT", 9000))
 
-# garantir que a pasta existe
+#se nao tem a pasta, cria
 if not os.path.exists(PASTA_COMPARTILHADA):
     os.makedirs(PASTA_COMPARTILHADA)
 
 
-# verificação dos arquivos locais
+#gera o hash pra monitorar o arquivo
 def gerar_hash_arquivo(caminho_arquivo):
     hash_md5 = hashlib.md5()
     try:
+        #picota o arquivo pra nao estourar a memoria
         with open(caminho_arquivo, "rb") as f:
             for chunk in iter(lambda: f.read(4096), b""):
                 hash_md5.update(chunk)
@@ -38,7 +36,7 @@ def gerar_hash_arquivo(caminho_arquivo):
 
 def escanear_pasta():
     estado_atual = {}
-    for raiz, _, arquivos in os.walk(PASTA_COMPARTILHADA):
+    for raiz, _,  arquivos in os.walk(PASTA_COMPARTILHADA):#tem o _ pq nao vai precisar usar as subpastas
         for arquivo in arquivos:
             caminho_completo = os.path.join(raiz, arquivo)
             caminho_relativo = os.path.relpath(caminho_completo, PASTA_COMPARTILHADA)
@@ -53,19 +51,17 @@ def escanear_pasta():
 
 
 def iniciar_servidor_tcp():
-    global meu_tcp_port
+    global minha_porta_tcp
     servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    servidor.bind(("", 0))
-    meu_tcp_port = servidor.getsockname()[1]
+    servidor.bind(("", 0)) #SO vai escolher a porta
+    minha_porta_tcp = servidor.getsockname()[1]
     servidor.listen(5)
 
-    print(f"[{meu_id}] Nó rodando. Escutando TCP na porta {meu_tcp_port}")
+    print(f"[{meu_id}] subiu na porta {minha_porta_tcp}")
 
     while True:
         cliente_socket, _ = servidor.accept()
-        threading.Thread(
-            target=lidar_com_cliente, args=(cliente_socket,), daemon=True
-        ).start()
+        threading.Thread(target=lidar_com_cliente, args=(cliente_socket,), daemon=True).start()
 
 
 def lidar_com_cliente(conexao):
@@ -73,28 +69,33 @@ def lidar_com_cliente(conexao):
         dados = conexao.recv(4096)
         if not dados:
             return
+        
         msg = json.loads(dados.decode("utf-8"))
-        if msg["acao"] == "GET_INDEX":
-            meu_index = escanear_pasta()
-            resposta = json.dumps({"acao": "INDEX", "dados": meu_index})
-            conexao.sendall(resposta.encode("utf-8"))
-        elif msg["acao"] == "GET_ARQUIVO":
+        
+        if msg["acao"] == "GET_ARQUIVO":
             caminho_relativo = msg["arquivo"]
             caminho_completo = os.path.join(PASTA_COMPARTILHADA, caminho_relativo)
             if os.path.exists(caminho_completo):
                 tamanho = os.path.getsize(caminho_completo)
-                conexao.sendall(json.dumps({"tamanho": tamanho}).encode("utf-8"))
-                time.sleep(0.1)
+                #transforma o json em bytes 
+                json_bytes = json.dumps({"tamanho": tamanho}).encode("utf-8")
+                #pega o tamanho do json
+                cabecalho = struct.pack('!I', len(json_bytes))
+                #manda o tamanho do cabecalho primeiro
+                conexao.sendall(cabecalho)
+                #depois manda o cabecalho
+                conexao.sendall(json_bytes)
+                #depois manda o arquivo
                 with open(caminho_completo, "rb") as f:
                     while chunk := f.read(4096):
                         conexao.sendall(chunk)
     except Exception as e:
-        print(f"Erro no servidor tcp {e}")
+        print(f"Erro no servidor tcp: {e}")
     finally:
         conexao.close()
 
 
-# parte de sincronização
+
 def baixar_arquivo(ip, porta, arquivo_relativo, tamanho_esperado):
     try:
         cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -103,8 +104,13 @@ def baixar_arquivo(ip, porta, arquivo_relativo, tamanho_esperado):
         req = json.dumps({"acao": "GET_ARQUIVO", "arquivo": arquivo_relativo})
         cliente.sendall(req.encode("utf-8"))
 
-        resp = cliente.recv(1024)
-        info = json.loads(resp.decode("utf-8"))
+        #vai receber o tamanho do cabecalho da outra funcao
+        tamanho_cabecalho = cliente.recv(4)
+        if not tamanho_cabecalho or len(tamanho_cabecalho)<4:
+            print("cabecalho nao chegou todo")
+        tamanho_json = struct.unpack('!I', tamanho_cabecalho)[0]
+        resposta = cliente.recv(tamanho_json)
+        info = json.loads(resposta.decode("utf-8"))
 
         if info.get("tamanho") == tamanho_esperado:
             caminho_salvamento = os.path.join(PASTA_COMPARTILHADA, arquivo_relativo)
@@ -140,30 +146,34 @@ def monitor_eventos():
             if arq in estado_anterior and estado_atual[arq]['hash']!=estado_anterior[arq]['hash']:
                 arquivos_modificados.append(arq)
                 
-        renomeados=[]
+        arquivos_renomeados=[]
         for deletado in arquivos_deletados[:]:
             for novo in arquivos_novos[:]:
                 if estado_anterior[deletado]['hash'] == estado_atual[novo]['hash']:
-                    renomeados.append((deletado, novo))
+                    arquivos_renomeados.append((deletado, novo))
                     arquivos_deletados.remove(deletado)
                     arquivos_novos.remove(novo)
                     break
         eventos_tracker = []
-        for antigo, novo in renomeados:eventos_tracker.append({"acao": "RENAME", "arquivo": antigo, "novo_nome": novo})
-        for arq in arquivos_deletados: eventos_tracker.append({"acao": "DELETE", "arquivo": arq})
-        for arq in arquivos_novos: eventos_tracker.append({"acao": "CREATE", "arquivo": arq, "tamanho": estado_atual[arq]['tamanho']})
-        for arq in arquivos_modificados: eventos_tracker.append({"acao": "MODIFY", "arquivo": arq, "tamanho": estado_atual[arq]['tamanho']})
+        for antigo, novo in arquivos_renomeados:
+            eventos_tracker.append({"acao": "RENAME", "arquivo": antigo, "novo_nome": novo})
+        for arq in arquivos_deletados: 
+            eventos_tracker.append({"acao": "DELETE", "arquivo": arq})
+        for arq in arquivos_novos: 
+            eventos_tracker.append({"acao": "CREATE", "arquivo": arq, "tamanho": estado_atual[arq]['tamanho']})
+        for arq in arquivos_modificados: 
+            eventos_tracker.append({"acao": "MODIFY", "arquivo": arq, "tamanho": estado_atual[arq]['tamanho']})
         
         for evento in eventos_tracker:
             evento["remetente"] = meu_id
-            evento["tcp_port"] = meu_tcp_port 
+            evento["tcp_port"] = minha_porta_tcp 
             evento["ip"] = socket_tracker_global.getsockname()[0]
             try:
                 msg = json.dumps(evento)
                 socket_tracker_global.sendall(msg.encode('utf-8'))
                 print(f"[{meu_id}] Enviei evento para o Tracker: {evento['acao']} em '{evento['arquivo']}'")
             except Exception as e:
-                pass
+                print(f"Erro ao enviar evento: {e}")
         estado_anterior = estado_atual
  
 def escutar_tracker():
@@ -208,6 +218,7 @@ def escutar_tracker():
                 except Exception as e:
                     print(f"[{meu_id}] Erro ao tentar fazer {acao} no arquivo {arquivo}: {e}")   
         except Exception as e:
+            print(f"Erro na conexao com o tracker: {e}")
             time.sleep(5)
         finally:
             cliente_tracker.close()
